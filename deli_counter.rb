57308ff9f7b3599bb5a96496a6f8f8c5d51cab98
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 17:33:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 katz_deli=[]
 def line (katz_deli)
 number_in_line=[];
@@ -25,4 +26,23 @@ def now_serving(katz_deli)
    puts "Currently serving #{katz_deli[0]}."
    katz_deli.shift
  end
+=======
+def line(katz_deli)
+  if katz_deli 
+  puts "The line is currently empty."
+end
+
+end
+
+
+def take_a_number
+
+
+end
+
+
+def now_serving
+
+
+>>>>>>> 0feb73e761266e8bee1bee37e94e07be5577b4fc
 end
